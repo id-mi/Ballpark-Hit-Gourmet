24aa7b6d17f334e1c1cf26eb4b95850c594156ed
id-mi <213282127+id-mi@users.noreply.github.com>
2025-07-10 17:08:08 +0900

    閲覧機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,14 @@ class PostsController < ApplicationController
     redirect_to '/top'
   end
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:place_category_id, :genre_category_id, :gourme_name, :shop_name, :price, :content)
