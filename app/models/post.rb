ddc2b46d13c417eab96179be949c27d97e620707
id-mi <213282127+id-mi@users.noreply.github.com>
2025-07-17 15:58:12 +0900

    投稿時のバリデーションを日本語に修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,13 +6,13 @@ class Post < ApplicationRecord
   belongs_to :place_category, optional: true
   belongs_to :genre_category, optional: true
 
-  validates :place_category_id, presence: { message: "を選択してください"}
-  validates :genre_category_id, presence: { message: "を選択してください"}
-  validates :gourme_name, presence: { message: "を入力してください"}
-  validates :shop_name, presence: { message: "を入力してください"}
-  validates :price, presence: { message: "を入力してください"}
-  validates :content, presence: { message: "を入力してください"}, length: { maximum: 150 }
-  validates :image, presence: { message: "を選択してください"}
+  validates :place_category_id, presence: true
+  validates :genre_category_id, presence: true
+  validates :gourme_name, presence: true
+  validates :shop_name, presence: true
+  validates :price, presence: true
+  validates :content, presence: true, length: { maximum: 150 }
+  validates :image, presence: true
 
   enum status: { published: 0, draft: 1 }
 
