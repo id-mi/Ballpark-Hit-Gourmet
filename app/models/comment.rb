55cffe8fddb51e60ab262f4724d8a34370886392
id-mi <213282127+id-mi@users.noreply.github.com>
2025-07-19 12:01:14 +0900

    コメント投稿時のバリデーションを日本語に修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post 
   
-  validates :content, presence: { message: "を入力してください"}, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 100 }
 end
